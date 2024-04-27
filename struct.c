9685d319cbef26858b6d9eab4aed24f659822827
dangwal-coding <bca23067@glbim.ac.in>
2024-04-27 21:27:23 +0530

    edits modify

--- struct.c
+++ struct.c
@@ -3,31 +3,39 @@ struct student
 {
     int roll_no;
     char name[20];
-    char father_name[20]; 
+    char fathername[40]; 
     int age;
+    int marks;
     char city[30];
 };
 int main(){
     struct student list[100];
+    // input students numbers
+    int n;
+    printf("no of students\n");
+    scanf("%d",&n);
     //input student details
-    for(int i=0;i<2;i++){
+    for(int i=0;i<n;i++){
         printf("student roll no.\n");
         scanf("%d",&list[i].roll_no);
         printf("student name\n");
         scanf("%s",list[i].name);
         printf("student father name\n");
-        scanf("%s",list[i].father_name);
+        scanf("%s",list[i].fathername);
         printf("student age\n");
         scanf("%d",&list[i].age);
+         printf("student marks\n");
+        scanf("%d",&list[i].marks);
         printf("student city\n");
         scanf("%s",list[i].city);
     }
-      for(int i=0;i<2;i++){
-        printf("student roll no. %d",list[i].roll_no);
-        printf("student name %s",list[i].name);
-        printf("student Father Name %s",list[i].father_name);
-        printf("student age %d",list[i].age);
-        printf("student city %s",list[i].city);
+      for(int i=0;i<n;i++){
+        printf("student roll no. %d\n",list[i].roll_no);
+        printf("student name %s\n",list[i].name);
+        printf("student Father Name %s\n",list[i].fathername);
+        printf("student age %d\n",list[i].age);
+        printf("student marks %d\n",list[i].marks);
+        printf("student city %s\n",list[i].city);
         printf("\n");
     }
 return 0;
